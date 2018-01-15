5405d6b1768dc33c2f4c2ad232cc5c5ab3a8cac8
Legenda374 <okssartur@gmail.com>
2018-01-15 20:18:43 +0200

    Start project

---
+++ SpellingChecker/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace SpellingChecker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.CheckButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(494, 295);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // CheckButton
+            // 
+            this.CheckButton.Location = new System.Drawing.Point(431, 313);
+            this.CheckButton.Name = "CheckButton";
+            this.CheckButton.Size = new System.Drawing.Size(75, 23);
+            this.CheckButton.TabIndex = 1;
+            this.CheckButton.Text = "Check";
+            this.CheckButton.UseVisualStyleBackColor = true;
+            this.CheckButton.Click += new System.EventHandler(this.CheckButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(518, 343);
+            this.Controls.Add(this.CheckButton);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Spelling Checker";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button CheckButton;
+    }
+}
+
---
+++ SpellingChecker/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Net;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace SpellingChecker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent(); 
+        }
+
+        private string request(string Data)
+        {
+            WebRequest req = WebRequest.Create("https://speller.yandex.net/services/spellservice/checkText?text=" + Data.Replace(' ', '+'));
+            WebResponse resp = req.GetResponse();
+            Stream stream = resp.GetResponseStream();
+            StreamReader sr = new StreamReader(stream);
+            string Out = sr.ReadToEnd();
+            sr.Close();
+            return Out;
+        }
+
+        private void CheckButton_Click(object sender, EventArgs e)
+        {
+            XDocument response = XDocument.Parse(request(richTextBox1.Text));
+            //var a = response.Root.Elements("error").Elements("word");
+            //a.XmlText();
+
+            string[] dic = new[] { }; // Слова сюда
+
+            for (int i = 0; i < dic.Length; i++)
+            {
+                int index = richTextBox1.Text.IndexOf(dic[i]);
+
+                richTextBox1.Select(index, dic[i].Length);
+                richTextBox1.SelectionFont = new Font(richTextBox1.SelectionFont, FontStyle.Underline);
+                richTextBox1.SelectionColor = Color.Red;
+            }
+        }
+    }
+}
